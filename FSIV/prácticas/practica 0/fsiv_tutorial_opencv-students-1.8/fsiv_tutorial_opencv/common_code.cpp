373f8794d22d31f9c1a93f655f06a6729d373055
CarlosDavidLopezHinojosa <carloscordobalopez2@gmail.com>
2024-09-18 14:44:35 +0200

    18/09/2024

--- FSIV/prácticas/practica 0/fsiv_tutorial_opencv-students-1.8/fsiv_tutorial_opencv/common_code.cpp
+++ FSIV/prácticas/practica 0/fsiv_tutorial_opencv-students-1.8/fsiv_tutorial_opencv/common_code.cpp
@@ -11,14 +11,47 @@ fsiv_find_min_max_loc_1(cv::Mat const& input,
     //! TODO: do a rows/cols scanning to find the first min/max values. 
     // Hint: use cv::split to get the input image channels.
 
+    std::vector<cv::Mat> channels;
+    cv::split(input,channels);
 
+    cv::uint8_t min, max;
+    cv::Point min_point, max_point;
 
+    for( auto channel : channels)
+    {   
+        min = 255;
+        max = 0;
+        for (size_t row = 0; row < channel.rows; row++)
+        {
+            for (size_t col = 0; col < channel.cols; col++)
+            {
+                const u_char pixel = channel.at<uchar>(row,col);
+
+                if (min > pixel)
+                {
+                    min = pixel;
+                    min_point = cv::Point(col,row);
+                }
+
+                if (max < pixel)
+                {
+                    max = pixel;
+                    max_point = cv::Point(col,row);
+                }
+            }
+        } 
+
+        min_v.push_back(min);
+        max_v.push_back(max);
+        min_loc.push_back(min_point);
+        max_loc.push_back(max_point);
+    }
     //
 
-    CV_Assert(input.channels()==min_v.size());
-    CV_Assert(input.channels()==max_v.size());
-    CV_Assert(input.channels()==min_loc.size());
-    CV_Assert(input.channels()==max_loc.size());
+    CV_Assert(size_t(input.channels())==min_v.size());
+    CV_Assert(size_t(input.channels())==max_v.size());
+    CV_Assert(size_t(input.channels())==min_loc.size());
+    CV_Assert(size_t(input.channels())==max_loc.size());
 }
 
 void 
@@ -29,15 +62,28 @@ fsiv_find_min_max_loc_2(cv::Mat const& input,
 
     //! TODO: Find the first min/max values using cv::minMaxLoc function.    
     // Hint: use cv::split to get the input image channels.
+    std::vector<cv::Mat> channels;
+    cv::split(input,channels);
 
+    double min, max;
+    cv::Point min_point, max_point;
 
+    for( auto channel : channels)
+    {           
+        cv::minMaxLoc(channel,&min,&max,&min_point,&max_point);
+
+        min_v.push_back(min);
+        max_v.push_back(max);
+        min_loc.push_back(min_point);
+        max_loc.push_back(max_point);
+    }
 
     //
 
-    CV_Assert(input.channels()==min_v.size());
-    CV_Assert(input.channels()==max_v.size());
-    CV_Assert(input.channels()==min_loc.size());
-    CV_Assert(input.channels()==max_loc.size());
+    CV_Assert(size_t(input.channels())==min_v.size());
+    CV_Assert(size_t(input.channels())==max_v.size());
+    CV_Assert(size_t(input.channels())==min_loc.size());
+    CV_Assert(size_t(input.channels())==max_loc.size());
 
 }
 
